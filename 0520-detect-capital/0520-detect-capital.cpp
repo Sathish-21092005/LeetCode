ae50aa079a1270fd5f54090af07b72a746079792
SATHISH D <smartkd061@gmail.com>
2025-06-04 21:01:14 +0530

    Time: 0 ms (100%), Space: 8.2 MB (51.39%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
           bool allUpper = true;
-    bool allLower = true;
+          bool allLower = true;
 
     for (char c : word) {
         if (!isupper(c)) allUpper = false;
         if (!islower(c)) allLower = false;
     }
-
-    // Check if first letter is uppercase and rest are lowercase
     bool firstUpperRestLower = isupper(word[0]);
     for (int i = 1; i < word.length(); i++) {
-        if (!islower(word[i])) {
+        if (isupper(word[i])) {
             firstUpperRestLower = false;
             break;
         }
