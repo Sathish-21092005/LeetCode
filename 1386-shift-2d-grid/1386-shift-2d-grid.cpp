e5724d017192a3139ebc4426f7d709e01a943b55
SATHISH D <smartkd061@gmail.com>
2025-06-06 14:39:00 +0530

    Time: 0 ms (100%), Space: 18.1 MB (91.5%) - LeetHub

--- 1386-shift-2d-grid/1386-shift-2d-grid.cpp
+++ 1386-shift-2d-grid/1386-shift-2d-grid.cpp
@@ -8,9 +8,9 @@ public:
 
         vector<vector<int>>result(m,vector<int>(n));
         
-        for(int i=0;i<m;++i)
+        for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;++j)
+            for(int j=0;j<n;j++)
             {
                
             int oldindex= i*n+j;
