1679f15975e4fb8095b6e75d91f6131bf818c0cd
SATHISH D <smartkd061@gmail.com>
2025-06-06 15:39:17 +0530

    Time: 0 ms (100%), Space: 10 MB (84.4%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int total=0;
+        int prev=0;
+
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            int value=0;
+            int ch=s[i];
+
+            if(ch=='I')value=1;
+            else if(ch=='V')value=5;
+            else if(ch=='X')value=10;
+            else if(ch=='L')value=50;
+            else if(ch=='C')value=100;
+            else if(ch=='D')value=500;
+            else if(ch=='M')value=1000;
+
+            if(value<prev){
+                total-=value;
+            }else{
+                total+=value;
+            }
+            prev=value;
+        }return total;
+    }
+};
