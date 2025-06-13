ba5bb7fd8e28a942580846a6514dabbbfa98dbe6
SATHISH D <smartkd061@gmail.com>
2025-06-13 12:45:04 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<int> count(n,1);
-        for(int i=1;i<m;++i)
+        for(int i=1;i<m;i++)
         {
-            for(int j=1;j<n;++j)
+            for(int j=1;j<n;j++)
             {
                 count[j]+=count[j-1];
             }
